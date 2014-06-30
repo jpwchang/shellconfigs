027422428a11eda8af0a6ee102bdfad85e01e1cc
jonathan <jonathan@localhost.localdomain>
2014-06-30 09:07:08 -0700

    Made extensions into a shell script

--- extensions.sh
+++ extensions.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/bash
+
+cd ~
+
 git clone git://github.com/tarruda/zsh-autosuggestions ~/.zsh-autosuggestions
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
 git clone https://github.com/zsh-users/zsh-history-substring-search.git
